92a3030c3d5be7fbab29ef341e10314c7d284559
AAPrikhodko <prand85@yandex.ru>
2021-01-15 15:34:06 +0300

    added buttons to the table

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import firebase from 'firebase'
 import "./home.css"
+import {Button, Table} from "react-bootstrap";
 
 
 const Home = () => {
@@ -12,7 +13,10 @@ const Home = () => {
     }, []);
 
     return (
-        <table className="table">
+        <div className="container-fluid text-right">
+                <Button className="badge-pill btn-success m-2 ">ADD USER</Button>
+            <div className="row">
+                <Table className="table" striped bordered hover responsive>
                     <thead>
                     <tr>
                         <th scope="col">Name</th>
@@ -22,24 +26,31 @@ const Home = () => {
                         <th scope="col">Career</th>
                         <th scope="col">DateOfCreate</th>
                         <th scope="col">DateOfChange</th>
+                        <th scope="col">Actions</th>
                     </tr>
                     </thead>
-                <tbody>
-                {(users !== undefined) && users.map(user => (
-                    <tr>
-                        <td>{user.name}</td>
-                        <td>{user.lastname}</td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>{user.career}</td>
-                        <td>{user.dateOfCreate}</td>
-                        <td>{user.dateOfChange}</td>
-                    </tr>
-                ))
-                }
-                </tbody>
-            </table>
-            )
+                    <tbody>
+                    {(users !== undefined) && users.map(user => (
+                        <tr>
+                            <td>{user.name}</td>
+                            <td>{user.lastname}</td>
+                            <td>{user.email}</td>
+                            <td>{user.phone}</td>
+                            <td>{user.career}</td>
+                            <td>{user.dateOfCreate}</td>
+                            <td>{user.dateOfChange}</td>
+                            <td className="container-fluid text-right">
+                                <Button className="badge-pill btn-primary m-2">EDIT</Button>
+                                <Button className="badge-pill btn-danger m-2">DELETE</Button>
+                            </td>
+                        </tr>
+                    ))
+                    }
+                    </tbody>
+                </Table>
+            </div>
+        </div>
+    )
 }
 
 export default Home
